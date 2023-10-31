358adb228857b20ceb95296b3368b955946bd8fa
anu4619 <anushka4619@gmail.com>
2023-10-31 22:44:43 +0530

    Inverted Triangle Pattern

---
+++ practicals/inverted_triangle.php
@@ -0,0 +1,11 @@
+<?php
+$rows = 5; // Change the number of rows as per your requirement
+
+for ($i = $rows; $i >= 1; $i--) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "\n";
+}
+?>
+
