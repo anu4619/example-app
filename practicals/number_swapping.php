f208c2c78cb72fcd0244b4cbfd13190de723e240
anu4619 <anushka4619@gmail.com>
2023-10-31 19:46:29 +0530

    Add number swapping code

---
+++ practicals/number_swapping.php
@@ -0,0 +1,17 @@
+<?php
+// Define two numbers
+$a = 5;
+$b = 10;
+
+// Display the numbers before swapping
+echo "Before swapping: a = " . $a . ", b = " . $b . "<br>";
+
+// Swap the values using a temporary variable
+$temp = $a;
+$a = $b;
+$b = $temp;
+
+// Display the numbers after swapping
+echo "After swapping: a = " . $a . ", b = " . $b;
+?>
+
