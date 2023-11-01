6469e541495d4f20b70aab01d49d546c65b4c129
anu4619 <anushka4619@gmail.com>
2023-11-01 20:32:03 +0530

    Inverted triangle pattern(readable and maintainable)

--- practicals/inverted_triangle.php
+++ practicals/inverted_triangle.php
@@ -1,11 +1,14 @@
 <?php
-$rows = 5; // Change the number of rows as per your requirement
+$rows = 5; // Define the number of rows for the inverted right triangle
 
 for ($i = $rows; $i >= 1; $i--) {
+    // Loop for each row, starting from the top and going downwards
     for ($j = 1; $j <= $i; $j++) {
+        // Loop to print asterisks (*) for each row
         echo "*";
     }
-    echo "\n";
+    echo "\n"; // Add a newline after each row
 }
 ?>
+
 
