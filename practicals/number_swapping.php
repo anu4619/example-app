ca4416a2f68b297143aaf3587fc2bbdfa4d8b0c8
anu4619 <anushka4619@gmail.com>
2023-11-01 20:24:33 +0530

    Swapping two numbers(readable and maintainable)

--- practicals/number_swapping.php
+++ practicals/number_swapping.php
@@ -4,7 +4,7 @@ $a = 5;
 $b = 10;
 
 // Display the numbers before swapping
-echo "Before swapping: a = " . $a . ", b = " . $b . "<br>";
+echo "Before swapping: a = " . $a . ", b = " . $b . "\n";
 
 // Swap the values using a temporary variable
 $temp = $a;
