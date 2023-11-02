b9895138f5cd60adc1030dcebf6f5e580a262305
anu4619 <anushka4619@gmail.com>
2023-11-02 23:13:33 +0530

    Add unit test case for Practical class

---
+++ tests/Unit/PracticalTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use practicals\practical; // Include the Practical class properly
+
+require(__DIR__ . '/../../practicals/practical.php');
+
+class PracticalTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function testAdd(): void
+    {
+        $this->assertEquals(practical::add(1, 3), 4);
+    }
+}
+
